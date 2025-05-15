925a0b9ffb77fa556e767f06b5af7a282edc9528
darthviner666 <denvviner.work@gmail.com>
2025-05-15 23:38:54 +1000

    new request method added

---
+++ src/main/java/com/framework/api/ApiRequests.java
@@ -0,0 +1,9 @@
+package com.framework.api;
+
+import java.util.Map;
+
+public class ApiRequestsBuilder {
+    public static ApiRequestsBuilder addHeaders(Map<String,String> headers) {
+        
+    }
+}
