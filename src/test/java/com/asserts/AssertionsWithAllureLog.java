ea9eaf67211346762ce5feb956aef87ae0dc1a3c
darthviner666 <denvviner.work@gmail.com>
2025-05-20 16:14:57 +1000

    assertions with allure log added

---
+++ src/test/java/com/asserts/AssertionsWithAllureLog.java
@@ -0,0 +1,4 @@
+package com.asserts;
+
+public class AssertionsWithAllureLog {
+}
--- src/test/java/com/ui/LoginTest.java
+++ src/test/java/com/ui/LoginTest.java
@@ -1,16 +1,13 @@
 package com.ui;
 
 import com.TestBase;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginTest extends TestBase {
-    @Test
-    @DisplayName("Successful login")
     void successfulLoginTest() {
         open("/login");
         $("#username").setValue("standard_user");
