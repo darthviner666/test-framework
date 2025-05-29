f1804b932adfb5a17db49c7b4b47846d4d5c7218
darthviner666 <denvviner.work@gmail.com>
2025-05-29 16:21:04 +1000

    files added

--- src/test/java/com/steps/RegistrationSteps.java
+++ src/test/java/com/steps/RegistrationSteps.java
@@ -1,4 +1,11 @@
 package com.steps;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class RegistrationSteps {
+    @Given("Пользователь находится на странице регистрации")
+    public void navigateToRegistrationPage() {
+         open("/register");
+        // Логика для навигации на страницу регистрации
+    }
 }
